1d6172a861806f7851b95ceb06e95d77f529978f
serj <sergeykalenikov@gmail.com>
2021-02-04 14:06:46 +0300

    6.4. Слово начинается с ... [#461857]

---
+++ src/ru/job4j/array/ArrayChar.java
@@ -0,0 +1,13 @@
+package ru.job4j.array;
+
+public class ArrayChar {
+    public static boolean startsWith(char[] word, char[] pref) {
+        int minLength = Math.min(pref.length, word.length);
+        for (int i = 0; i < minLength; i++) {
+            if (word[i] != pref[i]) {
+                return false;
+            }
+        }
+        return true;
+    }
+}
