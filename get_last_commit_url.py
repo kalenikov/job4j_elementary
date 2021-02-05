5766acce79b4ac774ca62e4065620528680d90dd
serj <sergeykalenikov@gmail.com>
2021-02-05 11:33:52 +0300

    change commits_per_page

--- get_last_commit_url.py
+++ get_last_commit_url.py
@@ -9,7 +9,7 @@ url = f'https://api.github.com/repos/{owner}/{repo}/commits?per_page=10'
 response = urlopen(url).read()
 data = json.loads(response.decode())
 
-for commit in data:
+for commit in data[::-1]:
     print('--------------')
     # print(commit['commit']['message'], commit['commit']['author']["date"])
     print(commit['commit']['message'], commit['html_url'])
